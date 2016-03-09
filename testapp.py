bfedd1aaac93e38942107979da14b81c4083280f
Administrator <admin.example.com>
2016-03-09 15:15:37 +0200

    Commiting a working minimalistic flask app

--- testapp.py
+++ testapp.py
@@ -1,6 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+DEBUG = True
 
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
 if __name__ == '__main__':
     app.run()
